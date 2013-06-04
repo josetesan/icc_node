f93564e955790c76d9ecedb1d9f8202fcfd316b5
muallin@gmail.com <muallin@gmail.com>
2013-06-04 17:18:36 +0200

    Errors :-(

--- icc_server_redis.js
+++ icc_server_redis.js
@@ -27,7 +27,7 @@ function debit(response,msisdn , content_code) {
     	} else {
 			var amount = data.split(":")[1];
 			console.log("Debiting '%s'  for '%s'", msisdn, amount);
-			client.get('customer:'+msisdn, function ( err, data) {
+			client.get('customer:234'+msisdn, function ( err, data) {
 				response.writeHead(200, {'Content-Type':'text/xml'});
 				if (err) {
 						console.log("User '%s' not found", msisdn);
@@ -39,11 +39,11 @@ function debit(response,msisdn , content_code) {
 					var balance = data.split(":")[1];
 					if (parseInt(balance) >= parseInt(amount) ) {
 						var newBalance = parseInt(balance) - parseInt(amount);
-						client.set('customer:'+msisdn, 'balance:'+newBalance);
+						client.set('customer:234'+msisdn, 'balance:'+newBalance);
 						console.log("User '%s' new balance is '%s'", msisdn, newBalance);
 						response.write('<?xml version="1.0"?><!DOCTYPE cp_reply SYSTEM "cp_reply.dtd">');
 						response.write('<cp_reply><cp_id>PEngine</cp_id><cp_transaction_id>test-'+parseInt(Math.random()*100000000)+'</cp_transaction_id>');
-						response.write('<op_transaction_id>'+parseLong(Math.random()*9999999999999)+'</op_transaction_id>');
+						response.write('<op_transaction_id>'+parseInt(Math.random()*999999999)+'</op_transaction_id>');
 						response.write('<service_units kind="Granted">');
 						response.write('<svc_unit u_type="0">1</svc_unit>');
 						response.write('</service_units>');
@@ -67,7 +67,7 @@ function debit(response,msisdn , content_code) {
 
 function getBalance(response,msisdn) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        client.get('customer:'+msisdn, function (err, data) {
+        client.get('customer:234'+msisdn, function (err, data) {
         if (err) {
 			response.write(0);    		
         } else {
