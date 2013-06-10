03c810e7ecfd7830297def96fb295862b79106f9
muallin@gmail.com <muallin@gmail.com>
2013-06-10 13:29:37 +0200

    User not found error was not handled properly

--- icc_server_redis.js
+++ icc_server_redis.js
@@ -36,35 +36,43 @@ function debit(response,msisdn , content_code) {
 			client.get('customer:234'+msisdn, function ( err, data) {
 				response.writeHead(200, {'Content-Type':'text/xml'});
 				if (err) {
+					log.write(dateFormat()+"| Error on finding user "+msisdn+"\n");
+					response.write('<?xml version="1.0"?><!DOCTYPE cp_reply SYSTEM "cp_reply.dtd">');
+					response.write('<cp_reply><cp_id>PEngine</cp_id><cp_transaction_id>test-'+parseInt(Math.random()*100000000)+'</cp_transaction_id>');
+					response.write('<result>999/result>');
+					response.write('</cp_reply>');
+				} else {
+					if (data) {
+						var balance = data.split(":")[1];
+						if (parseInt(balance) >= parseInt(amount) ) {
+							var newBalance = parseInt(balance) - parseInt(amount);
+							client.set('customer:234'+msisdn, 'balance:'+newBalance);
+							log.write(dateFormat() +"| User "+msisdn+" new balance is "+newBalance+"\n");
+							response.write('<?xml version="1.0"?><!DOCTYPE cp_reply SYSTEM "cp_reply.dtd">');
+							response.write('<cp_reply><cp_id>PEngine</cp_id><cp_transaction_id>test-'+parseInt(Math.random()*100000000)+'</cp_transaction_id>');
+							response.write('<op_transaction_id>'+parseInt(Math.random()*999999999)+'</op_transaction_id>');
+							response.write('<service_units kind="Granted">');
+							response.write('<svc_unit u_type="0">1</svc_unit>');
+							response.write('</service_units>');
+							response.write('<cost_information>'+amount+'</cost_information>');
+							response.write('<result>0</result>');
+							response.write('</cp_reply>');
+						} else {
+							log.write(dateFormat()+"| User "+msisdn+"  has not enough money, as balance is "+balance+"\n");
+							response.write('<?xml version="1.0"?><!DOCTYPE cp_reply SYSTEM "cp_reply.dtd">');
+							response.write('<cp_reply><cp_id>PEngine</cp_id><cp_transaction_id>test-'+parseInt(Math.random()*100000000)+'</cp_transaction_id>');
+							response.write('<result>10</result>');
+							response.write('</cp_reply>');
+						}
+				  } else {
 					log.write(dateFormat()+"| User "+msisdn+" not found\n");
 					response.write('<?xml version="1.0"?><!DOCTYPE cp_reply SYSTEM "cp_reply.dtd">');
 					response.write('<cp_reply><cp_id>PEngine</cp_id><cp_transaction_id>test-'+parseInt(Math.random()*100000000)+'</cp_transaction_id>');
 					response.write('<result>201/result>');
 					response.write('</cp_reply>');
-				} else {
-					var balance = data.split(":")[1];
-					if (parseInt(balance) >= parseInt(amount) ) {
-						var newBalance = parseInt(balance) - parseInt(amount);
-						client.set('customer:234'+msisdn, 'balance:'+newBalance);
-						log.write(dateFormat() +"| User "+msisdn+" new balance is "+newBalance+"\n");
-						response.write('<?xml version="1.0"?><!DOCTYPE cp_reply SYSTEM "cp_reply.dtd">');
-						response.write('<cp_reply><cp_id>PEngine</cp_id><cp_transaction_id>test-'+parseInt(Math.random()*100000000)+'</cp_transaction_id>');
-						response.write('<op_transaction_id>'+parseInt(Math.random()*999999999)+'</op_transaction_id>');
-						response.write('<service_units kind="Granted">');
-						response.write('<svc_unit u_type="0">1</svc_unit>');
-						response.write('</service_units>');
-						response.write('<cost_information>'+amount+'</cost_information>');
-						response.write('<result>0</result>');
-						response.write('</cp_reply>');
-					} else {
-						log.write(dateFormat()+"| User "+msisdn+"  has not enough money, as balance is "+balance+"\n");
-						response.write('<?xml version="1.0"?><!DOCTYPE cp_reply SYSTEM "cp_reply.dtd">');
-						response.write('<cp_reply><cp_id>PEngine</cp_id><cp_transaction_id>test-'+parseInt(Math.random()*100000000)+'</cp_transaction_id>');
-						response.write('<result>10</result>');
-						response.write('</cp_reply>');
-					}
 				}
-				response.end();
+			  }
+			  response.end();
 			});
 	  } else {
 	       	response.writeHead(500, {'Content-Type':'text/plain'});
@@ -98,10 +106,16 @@ client.on("end", function() {
     console.log("Closing redis");
 });
 
-//http.on("close", function() {
-//    console.log(dateFormat()+"| Server closed");
-//    client.quit();
-//});
+/*
+var server = http.createServer(onRequest);
+
+server.on("close", function() {
+    console.log(dateFormat()+"| Server closed");
+    client.quit();
+});
+
+server.listen(8887);
+*/
 
 http.createServer(onRequest).listen(8887);
 console.log('Server running at http://127.0.0.1:8887');
